5d297def69c701768c05382153a767a150096a93
Jahongir <gjahongir2006@gmail.com>
2026-05-30 00:14:38 +0500

    Update page.tsx

--- my-app/app/posts/[id]/page.tsx
+++ my-app/app/posts/[id]/page.tsx
@@ -1,23 +1,17 @@
-// app/posts/[id]/page.tsx
-
 import PostDetail from './PostDetail'
-import axiosIns from '../../config/constant'
-
 
 export async function generateStaticParams() {
   try {
-    const res = await axiosIns.get('/posts')
-    const posts = res.data
-
+    const res = await fetch('https://ion-blog-site.onrender.com/api/posts')
+    const posts = await res.json()
     return posts.map((post: any) => ({
       id: String(post._id || post.id),
     }))
-  } catch (error) {
-    console.error('generateStaticParams xatosi:', error)
+  } catch {
     return []
   }
 }
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page() {
   return <PostDetail />
 }
